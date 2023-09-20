de128ee2ec294d859fc62211aee06c8e6549eec5
sajja-10 <138700931+sajja-10@users.noreply.github.com>
2023-09-21 00:53:04 +0545

    Update script.js

--- script.js
+++ script.js
@@ -116,17 +116,19 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 
 const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to the form submit button
-formBtn.addEventListener("click", function () {
-    // Open the user's default email client with the pre-filled email
-    location.href = "mailto:sajjaregmi4@gmail.com" +
-        "?subject=Feedback" +
-        "&body=Full Name: " + form.elements.fullname.value + "%0D%0A" +
-        "Email: " + form.elements.email.value + "%0D%0A" +
-        "Feedback: " + form.elements.message.value;
-});
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
 
 
 
